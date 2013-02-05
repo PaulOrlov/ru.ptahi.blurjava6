e31a43e312e0fa9dc7c1f4577c9229c3549c0ec3
Paul Orlov <paul.a.orlov@gmail.com>
2013-02-05 13:13:13 +0200

    Revert "first commit"
    This reverts commit 44a531ae798b4db0281c676745ce4ba6e8df0ca9.

--- src/ru/ptahi/blurjava/package-info.java
+++
@@ -1,8 +0,0 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-@TemplateRegistration(folder = "Other", content = "BlurJavaModuleTemplate.Extention")
-package ru.ptahi.blurjava;
-
-import org.netbeans.api.templates.TemplateRegistration;
