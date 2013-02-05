5dc6fdfa6bbd8a23aeb05dcc143942434c9d87df
Paul Orlov <paul.a.orlov@gmail.com>
2013-02-05 13:13:52 +0200

    again

---
+++ build/classes-generated/ru/ptahi/blurjava/Bundle.java
@@ -0,0 +1,27 @@
+package ru.ptahi.blurjava;
+/** Localizable strings for {@link ru.ptahi.blurjava}. */
+@javax.annotation.Generated(value="org.netbeans.modules.openide.util.NbBundleProcessor")
+class Bundle {
+    /**
+     * @return <i>Source</i>
+     * @see BlurJavaModuleDataObject
+     */
+    static String LBL_BlurJavaModule_EDITOR() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "LBL_BlurJavaModule_EDITOR");
+    }
+    /**
+     * @return <i>Files of BlurJavaModule</i>
+     * @see BlurJavaModuleDataObject
+     */
+    static String LBL_BlurJavaModule_LOADER() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "LBL_BlurJavaModule_LOADER");
+    }
+    /**
+     * @return <i>Visual</i>
+     * @see BlurJavaModuleVisualElement
+     */
+    static String LBL_BlurJavaModule_VISUAL() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "LBL_BlurJavaModule_VISUAL");
+    }
+    private void Bundle() {}
+}
