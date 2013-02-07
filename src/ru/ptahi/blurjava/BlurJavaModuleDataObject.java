2979899be04200d42849c13917332b548767624a
Paul Orlov <paul.a.orlov@gmail.com>
2013-02-07 17:06:23 +0200

    new

--- src/ru/ptahi/blurjava/BlurJavaModuleDataObject.java
+++ src/ru/ptahi/blurjava/BlurJavaModuleDataObject.java
@@ -4,6 +4,15 @@
  */
 package ru.ptahi.blurjava;
 
+import java.awt.color.ColorSpace;
+import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
+import java.awt.image.ComponentColorModel;
+import java.awt.image.DataBuffer;
+import java.awt.image.Raster;
+import java.awt.image.WritableRaster;
+import java.util.Hashtable;
+
 import java.awt.AWTEvent;
 import java.awt.Color;
 import java.awt.Graphics;
@@ -37,6 +46,7 @@ import org.openide.windows.WindowManager;
 import static com.googlecode.javacv.cpp.opencv_imgproc.*;
 import static com.googlecode.javacv.cpp.opencv_core.*;
 import com.jogamp.opencl.CLContext;
+import java.awt.image.DataBufferByte;
 import static java.lang.System.out;
 
 
@@ -153,6 +163,9 @@ class MYLAYERUI extends LayerUI<java.awt.Container> {
 
     }
 
+    BufferedImage asd;
+    Graphics2D asdg;
+    Timer t = new Timer();
     @Override
     public void paint(Graphics g, JComponent c) {
         int w = c.getWidth();
@@ -160,6 +173,31 @@ class MYLAYERUI extends LayerUI<java.awt.Container> {
 
         if (w == 0 || h == 0) {
             return;
+        }
+        
+        if (asd == null) {
+            asd =  new BufferedImage(
+                        new ComponentColorModel(
+                                ColorSpace.getInstance(ColorSpace.CS_sRGB),
+                                new int[] { 32 },
+                                true,
+                                true,
+                                ComponentColorModel.TRANSLUCENT,
+                                DataBuffer.TYPE_INT
+                                ),
+                       Raster.createInterleavedRaster(
+                                DataBuffer.TYPE_INT,
+                                w,
+                                h,
+                                w,
+                                0,
+                                new int[] { 2, 1, 0, 3 },
+                                null
+                                ),
+                        false,
+                        new Hashtable<Object, Object>()
+                    );
+            asdg = asd.createGraphics();
         }
 
         // Only create the offscreen image if the one we have
@@ -180,14 +218,26 @@ class MYLAYERUI extends LayerUI<java.awt.Container> {
         Graphics2D ig2 = mOffscreenImage.createGraphics();
         ig2.setClip(g.getClip());
         super.paint(ig2, c);
+        t.reset();
+        super.paint(asdg, c);
+        System.out.println("asdg: " + t.diffAndReset());
         ig2.dispose();
+        t.reset();
+        
+        
+        
+                
         srcImg.copyFrom(mOffscreenImage);
+        System.out.println("copyFrom: " + t.diffAndReset());
        
 //        destImg = srcImg.clone();
 //        BufferedImage dest = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
         
+        t.reset();
         GaussianBlur(srcImg, destImg, new CvSize(0, 0), 3d, 3d, BORDER_DEFAULT);
+        System.out.println("GaussianBlur: " + t.diffAndReset());
         destImg.copyTo(destOffscreenImage);
+        System.out.println("destImg.copyTo: " + t.diffAndReset());
         
 //        cv.allocate((int) w, (int) h);
 //        cv.copy(new PImage(mOffscreenImage));         //  Copies trailsImg into OpenCV buffer so we can put some effects on it
@@ -229,9 +279,11 @@ class MYLAYERUI extends LayerUI<java.awt.Container> {
         bMouseX = mousePoint.x;
         bMouseY = mousePoint.y;
         
+        System.out.println("drawImage: " + t.diffAndReset());
         g2.drawImage(destOffscreenImage, 0, 0, null);
         g2.setColor(Color.red);
         g2.fillRect(bMouseX, bMouseY, 20, 20);
+        System.out.println("fillRect: " + t.diffAndReset());
     }
 
     @Override
@@ -275,3 +327,175 @@ class MYLAYERUI extends LayerUI<java.awt.Container> {
         ((JLayer) c).setLayerEventMask(0);
     }
 };
+
+
+/* [LGPL] Copyright 2010, 2011 Gima
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/**
+ * Differential timer.
+ */
+class Timer {
+        
+        private long lastTick;
+
+        public Timer() {
+                lastTick = -1;
+        }
+        
+        /**
+         * Retrieve the difference from last reset.
+         * 
+         * @return Difference in milliseconds since last reset. If last call wasn't made, returns 0
+         */
+        public long diff() {
+                
+                long retDiff;
+                
+                if (lastTick == -1) {
+                        retDiff = 0;
+                }
+                else {
+                        retDiff = System.currentTimeMillis() - lastTick;
+                }
+                
+                return retDiff;
+        }
+        
+        /**
+         * Retrieve the difference from last reset and then reset.
+         * 
+         * @return Difference in milliseconds since last reset. If last call wasn't made, returns 0
+         */
+        public long diffAndReset() {
+                long retDiff = diff();
+                reset();
+                return retDiff;
+        }
+        
+        /**
+         * Reset to current timer.
+         */
+        public void reset() {
+                lastTick = System.currentTimeMillis();
+        }
+        
+}
+
+/* [LGPL] Copyright 2010, 2011 Gima
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+class ImageUtils {
+        
+        public static final int[] pixelOffsets_BGR;
+        public static final int[] pixelOffsets_BGRA;
+        public static final int[] pixelOffsets_GRAY;
+        
+        public static final int[] bpp_8;
+        public static final int[] bpp_24;
+        public static final int[] bpp_32;
+        
+        public static final ColorSpace colorSpace_sRGB;
+        public static final ColorSpace colorSpace_GRAY;
+        
+        public static final ComponentColorModel colorModel_Alpha32BPP;
+        public static final ComponentColorModel colorModel_24BPP;
+        public static final ComponentColorModel colorModel_8BPP;
+        
+        static {
+                pixelOffsets_BGR = new int[] { 2, 1, 0 };
+                pixelOffsets_BGRA = new int[] { 2, 1, 0, 3 };
+                pixelOffsets_GRAY = new int[] { 0 };
+                
+                bpp_8 = new int[] { 8 };
+                bpp_24 = new int[] { 8, 8, 8 };
+                bpp_32 = new int[] { 8, 8, 8, 8 };
+                
+                colorSpace_sRGB = ColorSpace.getInstance(ColorSpace.CS_sRGB);
+                colorSpace_GRAY = ColorSpace.getInstance(ColorSpace.CS_GRAY);
+
+                colorModel_Alpha32BPP = createComponentColorModel(
+                                colorSpace_sRGB,
+                                bpp_32,
+                                true,
+                                DataBuffer.TYPE_BYTE
+                                );
+                
+                colorModel_24BPP = createComponentColorModel(
+                                colorSpace_sRGB,
+                                bpp_24,
+                                false,
+                                DataBuffer.TYPE_BYTE
+                                );
+                
+                colorModel_8BPP = createComponentColorModel(
+                                colorSpace_GRAY,
+                                bpp_8,
+                                false,
+                                DataBuffer.TYPE_BYTE
+                                );
+        }
+        
+        /*
+         * common functions
+         */
+        
+        public static ComponentColorModel createComponentColorModel(ColorSpace colosSpace, int[] bits, boolean useAlpha, int dataType) {
+                return new ComponentColorModel(
+                                colosSpace,
+                                bits,
+                                useAlpha,
+                                useAlpha,
+                                useAlpha ? ComponentColorModel.TRANSLUCENT : ComponentColorModel.OPAQUE,
+                                dataType
+                                );
+        }
+
+        public static WritableRaster createInterleavedRaster(int width, int height, int bytesPerPixel, int[] pixelOffsets) {
+
+                return Raster.createInterleavedRaster(
+                                DataBuffer.TYPE_BYTE,
+                                width,
+                                height,
+                                width * bytesPerPixel,
+                                bytesPerPixel,
+                                pixelOffsets,
+                                null
+                                );
+        }
+
+        public static BufferedImage createBufferedImage(ColorModel colorModel, WritableRaster writableRaster) {
+                return new BufferedImage(
+                                colorModel,
+                                writableRaster,
+                                false,
+                                new Hashtable<Object, Object>()
+                                );
+        }
+
+}
